b573258eb54d76ba3dfd55b3db2594619da9aaa6
Vishnu p <palappillivishnu@gmail.com>
2026-08-01 00:00:42 +0530

    Add AWS provider and instance configuration

--- day1 /main.tf
+++ day1 /main.tf
@@ -1,4 +1,9 @@
-mmm
-
-
-  
+provider "aws" {
+    region = "ap-south-1"
+    }
+resource "aws_instance" "vishnu1" {
+  ami                     = "ami-00d2dbb426772b03a"
+  instance_type           = "t3.micro"
+  subnet_id     = "subnet-0e4b6a255106db804"
+  key_name = "vishnu"
+  }
